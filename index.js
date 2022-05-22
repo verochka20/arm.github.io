601a63cb0d72d860d165726343fc8134cbb1c0ac
verochka20 <76683529+verochka20@users.noreply.github.com>
2022-05-22 16:40:17 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Container, ChakraProvider, Heading, Flex, Spacer, Text, Stack, Divider, Link} from "@chakra-ui/react";
+import { Container, ChakraProvider, Heading, Flex, Spacer, Text, Stack, Divider, Link, Button, Box, ButtonGroup} from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import ararat from './images/ararat.png'
+import gora1 from './images/gora1.png'
+import gora2 from './images/gora2.png'
+import gora3 from './images/gora3.png'
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverAnchor,
+} from '@chakra-ui/react'
 function Well() {
   return (
     <ChakraProvider>
@@ -12,7 +26,17 @@ function Well() {
           Armenian Travel Guide
           </Heading>
           <Spacer />
-          <HamburgerIcon w="20px" h="20px" color="#FFFFFF" />
+          <Popover>
+  <PopoverTrigger>
+    <HamburgerIcon w="20px" h="20px" color="#FFFFFF">Trigger</HamburgerIcon>
+  </PopoverTrigger>
+  <PopoverContent>
+    <PopoverArrow />
+    <PopoverCloseButton />
+    <PopoverHeader>Confirmation!</PopoverHeader>
+    <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
+  </PopoverContent>
+</Popover>
         </Flex>
         <Spacer />
         <Container  backgroundImage={ararat} width="375px" height="179px">
@@ -43,7 +67,7 @@ function Well() {
           BARRIER
           </Text>
         </Flex>
-        <Flex>
+        <Flex display= 'flex' direction='row'>
           <Flex direction='row' p={4} >
           <Link href='https://tonkosti.ru/%D0%94%D0%BE%D1%81%D1%82%D0%BE%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%87%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8_%D0%90%D1%80%D0%BC%D0%B5%D0%BD%D0%B8%D0%B8' isExternal fontSize="md" color="#FFFFFF">
             Attractions
@@ -51,14 +75,29 @@ function Well() {
           <Divider orientation='horizontal' /> 
           </Flex>
           <Flex>
-
+          <Flex p={4} >
+          <Link href='https://travelask.ru/articles/samye-krasivye-goroda-armenii' isExternal fontSize="md" color="#FFFFFF">
+            Cities
+          </Link>
+          <Divider orientation='horizontal' /> 
+          </Flex>
           </Flex>
         </Flex>
-        
+        <Container>
+          <Heading size="lg" color="#FFFFFF">
+            Find something for yourself
+          </Heading>
+          <Flex>
+            <img src={gora1}/>
+            <img src={gora2}/>
+            <img src={gora3}/>
+          </Flex>
+        </Container>
       </Container>
     </ChakraProvider>
   );
 }
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
